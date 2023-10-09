8143116c0f6d2f860ae91a34b9f769fe31dd851a
Rahul A R <rahular@accubits.com>
2023-10-09 16:46:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,19 +22,22 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 def get_credentials():
     creds = None
-    if os.path.exists('token.pickle'):
+    try:
+        if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
     
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+    except as e:
+        pass
     
     return creds
 
